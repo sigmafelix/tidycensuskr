94bc1184b24c15a4a55b794b49f46fae73dc1281
Insang Song <sigmafelix@hotmail.com>
2025-09-08 23:31:49 +0900

    0.1.0 dev (2) - vignette 1: new chapter on adm2_code changes in 2022 (i.e., addition of 2 to the third digit in all rural districts) - districts sf data: further simplified and unified to EPSG:5179

--- R/datasets.R
+++ R/datasets.R
@@ -4,24 +4,27 @@
 #' and mortality in South Korea in 2010, 2015, and/or 2020. The availble
 #' years and variables depend on the type of data.
 #'
-#' @format A data frame with 4410 rows and 9 variables:
-#' \describe{
-#'   \item{adm1}{Name of the province-level (Sido) administrative unit}
-#'   \item{adm2}{Name of the district/municipal-level (Sigungu) administrative unit}
-#'   \item{adm2_code}{Code of the district/municipal-level (Sigungu) administrative unit}
-#'   \item{type}{Type of variable, e.g., "tax", "population", "mortality"}
-#'   \item{class1}{First-level classification of the variable depending on the type}
-#'   \item{class2}{Second-level classification of the variable depending on the type}
-#'   \item{unit}{Unit of measurement for the variable}
-#'   \item{value}{Value of the variable}
-#'   \item{year}{Year of the census data, e.g., 2010, 2015, or 2020}
-#' }
+#' @format A data frame with 5178 rows and 9 variables:
+#' @details
+#' * year Year of the census data, e.g., 2010, 2015, or 2020
+#' * adm1 Name of the province-level (Sido) administrative unit
+#' * adm2 Name of the district/municipal-level (Sigungu) administrative unit
+#' * adm2_code  Code of the district/municipal-level (Sigungu) administrative unit
+#' * type Type of variable, e.g., "tax", "population", "mortality"
+#' * class1 First-level classification of the variable depending on the type
+#' * class2 Second-level classification of the variable depending on the type
+#' * unit Unit of measurement for the variable
+#' * value  Value of the variable
+#'
 #' @note
 #' For temporal comparison, province names in adm1 field are
 #' standardized to the common names with no suffix in metropolitan cities
 #' and "-do" suffix in provinces.
 #' For example, "Seoul" instead of "Seoul Metropolitan City",
 #' and "Jeollabuk-do" instead of "Jeonbuk State".
+#' "KRW" in the unit field stands for South Korean Won.
+#' Values are as-is unless otherwise noted in the unit field
+#' (e.g., "per 100k population" or "million KRW").
 #' @source
 #' * KOSIS (Korean Statistical Information Service)
 #' @keywords datasets
