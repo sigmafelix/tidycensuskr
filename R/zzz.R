8ca3e6edc887c9892a173e80a6ec5c785ff198f6
Insang Song <sigmafelix@hotmail.com>
2025-10-01 16:17:13 +0900

    data package separation in progress - TODO: remove vignettes and edit README.Rmd

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,9 @@
     build_date <- file.info(desc_file)$mtime
     packageStartupMessage(
       sprintf(
-        "tidycensuskr %s (%s)",
+        "tidycensuskr %s (%s)\n
+        Please install the companion data package tidycensussfkr to use the district boundaries.\n
+        See `?install_companion` for details.\n",
         utils::packageVersion(pkgname),
         format(build_date, "%Y-%m-%d")
       )
