839162cc0e33041d30ad57b885ce2be3b96be64f
Insang Song <sigmafelix@hotmail.com>
2026-07-17 11:28:55 +0900

    0.2.9.9001 - anycensus() update - load_districts(): added level argument

--- R/load_districts.R
+++ R/load_districts.R
@@ -1,5 +1,6 @@
 #' Load district boundaries for a specific year
 #' @param year The year for which to load district boundaries (2010, 2015, or 2020)
+#' @param level The administrative level, one of "adm2" and "adm3"
 #' @return An `sf` object containing district boundaries for the specified year
 #' @importFrom sf st_read
 #' @note This function requires the `tidycensuskr.sf` package to be installed.
@@ -9,15 +10,20 @@
 #' @import sf
 #' @importFrom rlang is_installed
 #' @export
-load_districts <- function(year = 2020) {
+load_districts <- function(year = 2020, level = "adm2") {
+  requireNamespace("sf", quietly = TRUE)
   if (!year %in% c(2010, 2015, 2020)) {
     stop("Year must be one of 2010, 2015, or 2020")
   }
 
   if (!rlang::is_installed("tidycensuskr.sf")) {
-    stop("Package 'tidycensuskr.sf' is required for this function. Please install it.")
+    stop(
+      "Package 'tidycensuskr.sf' is required for this function.",
+      "Please install it by running:",
+      "install.packages('tidycensuskr.sf', repos = 'https://sigmafelix.r-universe.dev')"
+    )
   }
-  file_path <- system.file(sprintf("extdata/adm2_sf_%d.rds", year),
+  file_path <- system.file(sprintf("extdata/%s_sf_%d.rds", level, year),
     package = "tidycensuskr.sf"
   )
 
